54dce57e0ec7041512211f7940079b58e013bbdf
Sovan Roy <roysovan00@gmail.com>
2025-08-14 08:36:25 +0530

    added register module

--- src/app/pages/register/register/register.ts
+++ src/app/pages/register/register/register.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  imports: [FormsModule,CommonModule,RouterModule],
   selector: 'app-register',
-  imports: [],
   templateUrl: './register.html',
-  styleUrl: './register.css'
+  styleUrls: ['./register.css']
 })
 export class Register {
+  formData = {
+    name: '',
+    email: '',
+    password: '',
+    role: 'user'
+  };
 
+  constructor(private router: Router) {}
+
+  handleSubmit(event: Event) {
+    event.preventDefault();
+    console.log('Registration data:', this.formData);
+    // TODO: API call for registration
+    this.router.navigate(['/']);
+  }
 }
